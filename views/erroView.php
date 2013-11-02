e96e4cee11358085b88141b3b8d0abce892cc25e
jrj <jurij.nemanic@gmail.com>
2013-11-02 19:10:03 +0100

    errorView added

---
+++ views/erroView.php
@@ -0,0 +1,14 @@
+<?php
+
+// other stuff...
+
+<h1>Exception occured</h1>
+<br/>
+
+<?php 
+	foreach($exc_array as $key=>$value){
+	
+		echo "key: $key  value: $value"; 
+	}
+
+// ok blabla... + druga vsebina
